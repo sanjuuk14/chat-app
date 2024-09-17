08caf302a2d196dfd84a4d66dad23d544f747c87
sanjuuk14 <spanwar7296@gmail.com>
2024-09-17 21:46:34 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ import MessageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket.js/socket.js";
+import path from "path";
 app;
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,10 +21,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", MessageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-// root route http://localhost:5000/
-//   res.send("hello world !!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 server.listen(PORT, () => {
   connectToMongoDB();
